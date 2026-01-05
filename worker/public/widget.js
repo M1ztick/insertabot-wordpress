dc43fc7ccc620cd6d6abdb7002f7136f29aed497
Mistyk Media <support@insertabot.io>
2026-01-05 02:28:14 -0700

    Changed widget branding from insertabot.com to insertabot.io.

--- worker/public/widget.js
+++ worker/public/widget.js
@@ -258,7 +258,7 @@
         ${
           widgetConfig.show_branding
             ? `<div style="text-align: center; margin-top: 8px; font-size: 11px; color: #9ca3af;">
-               Powered by <a href="https://insertabot.com" target="_blank" style="color: ${widgetConfig.primary_color}; text-decoration: none;">Insertabot</a>
+               Powered by <a href="https://insertabot.io" target="_blank" style="color: ${widgetConfig.primary_color}; text-decoration: none;">Insertabot</a>
              </div>`
             : ""
         }
